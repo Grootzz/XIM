258223f43c3041756c434b169dc00e58971f8bed
Grootzz <noodle.ax@gmail.com>
2019-06-27 16:22:07 +0800

    实现加入群组命令:jg

--- xim-client/src/main/java/com/xim/client/console/JoinGroupConsoleCommand.java
+++ xim-client/src/main/java/com/xim/client/console/JoinGroupConsoleCommand.java
@@ -1,8 +1,10 @@
 package com.xim.client.console;
 
+import com.xim.common.protocol.req.CreateGroupRequestPacket;
 import com.xim.common.protocol.req.JoinGroupRequestPacket;
 import io.netty.channel.Channel;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -25,6 +27,17 @@ public class JoinGroupConsoleCommand implements ConsoleCommand {
 
     @Override
     public void exec(String statement, Channel channel) {
+        String trim = statement.trim();
+        String[] strings = trim.split(" ");
 
+        if (strings.length < 2) {
+            logger.info("输入参数个数错误");
+            return;
+        }
+        String groupId = strings[1];
+
+        JoinGroupRequestPacket joinGroupRequestPacket = new JoinGroupRequestPacket();
+        joinGroupRequestPacket.setGroupId(groupId);
+        channel.writeAndFlush(joinGroupRequestPacket);
     }
 }
